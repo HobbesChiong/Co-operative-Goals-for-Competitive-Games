f17de60ba862d76ca424d1d9305d39cbc3f6f6d4
Alex <metroid3d2@gmail.com>
2022-10-28 15:09:38 -0700

    Add app appbar XML

--- app/src/main/java/ca/cmpt276/iteration1/activities/GameTypeActivity.java
+++ app/src/main/java/ca/cmpt276/iteration1/activities/GameTypeActivity.java
@@ -1,5 +1,6 @@
 package ca.cmpt276.iteration1.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,13 @@ public class GameTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_type);
+
+        ActionBar ab = getSupportActionBar();
+
+        String appBarTitle = "Create new Game Configuration";
+
+
+        ab.setTitle(appBarTitle);
+
     }
 }
