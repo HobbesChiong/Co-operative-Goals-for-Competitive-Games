2b6c830b36f91339067f14f980cb85d4187ee966
ccw21 <ccw21@sfu.ca>
2022-10-28 16:44:16 -0700

    Added achievement variable and class level comment

--- app/src/main/java/ca/cmpt276/iteration1/model/PlayedGame.java
+++ app/src/main/java/ca/cmpt276/iteration1/model/PlayedGame.java
@@ -1,14 +1,23 @@
 package ca.cmpt276.iteration1.model;
+/*
+* The PlayedGame class is for storing played game's data.
+* type for game name
+* numberOfPlayers for #Players
+* score for the total score of the whole team game
+* achievement for the achievement that the team got according to their score.
+* */
 
 public class PlayedGame {
     private final String type;
     private final int numberOfPlayers;
     private final int score;
+    private final String achievement;
 
-    public PlayedGame(String type, int numberOfPlayers, int score) {
+    public PlayedGame(String type, int numberOfPlayers, int score, String achievement) {
         this.type = type;
         this.numberOfPlayers = numberOfPlayers;
         this.score = score;
+        this.achievement = achievement;
     }
 
     public String getType() {
@@ -21,5 +30,9 @@ public class PlayedGame {
 
     public int getScore() {
         return score;
+    }
+
+    public String getAchievement(){
+        return achievement;
     }
 }
