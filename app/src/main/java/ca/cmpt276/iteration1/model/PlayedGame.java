087f750bd2513395e42b34f2340e3cf0dfb3f04c
ccw21 <ccw21@sfu.ca>
2022-10-27 22:26:31 -0700

    Remove setters and set variables to final

--- app/src/main/java/ca/cmpt276/iteration1/model/PlayedGame.java
+++ app/src/main/java/ca/cmpt276/iteration1/model/PlayedGame.java
@@ -1,9 +1,9 @@
 package ca.cmpt276.iteration1.model;
 
 public class PlayedGame {
-    private String type;
-    private int numberOfPlayers;
-    private int score;
+    private final String type;
+    private final int numberOfPlayers;
+    private final int score;
 
     public PlayedGame(String type, int numberOfPlayers, int score) {
         this.type = type;
@@ -15,23 +15,11 @@ public class PlayedGame {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public int getNumberOfPlayers() {
         return numberOfPlayers;
     }
 
-    public void setNumberOfPlayers(int numberOfPlayers) {
-        this.numberOfPlayers = numberOfPlayers;
-    }
-
     public int getScore() {
         return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
     }
 }
