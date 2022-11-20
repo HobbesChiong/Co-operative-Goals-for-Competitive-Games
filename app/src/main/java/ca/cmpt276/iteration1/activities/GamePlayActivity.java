00b4ee75bbd6e9c906eaedb87614da8548e52209
Yejun Kang <yka92@sfu.ca>
2022-11-20 02:51:11 -0800

    Merge up-to-date master to branch

--- app/src/main/java/ca/cmpt276/iteration1/activities/GamePlayActivity.java
+++ app/src/main/java/ca/cmpt276/iteration1/activities/GamePlayActivity.java
@@ -361,7 +361,6 @@ public class GamePlayActivity extends AppCompatActivity implements PlayerScoreIn
         etPlayerCount.setText(String.valueOf(playerAmount));
 
         updateScoreTextView();
-
         setupGameInfoModels();
     }
 
